d5e6b80409935dd0fe9b22495eea47466dd24e75
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-07-28 18:44:00 +0300

    alx_book_store

---
+++ task_5.sql
@@ -0,0 +1,18 @@
+import mysql.connector
+
+try:
+
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        database = "alx_book_store"
+        )
+    cursor = connection.cursor()
+    cursor.execute("USE alx_book_store;")
+    cursor.execute("INSERT INTO Customers(customer_id, customer_name, customer_email, address) VALUES(1, "Cole Baidoo", "cbaidoo@sandtech.com", 123 "Happiness Ave")")
+    connection.commit()
+    print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error as e:
+    print(e)
