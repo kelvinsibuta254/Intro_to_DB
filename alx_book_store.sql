9a6ac5f47b34ebb7e2f6bb08596d052e9eb28fb1
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-07-28 18:26:41 +0300

    alx_book_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Books (
-    book_id INT AUTO INCREMENT PRIMARY KEY,
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
@@ -11,26 +11,26 @@ CREATE TABLE Books (
 )
 
 CREATE TABLE Authors (
-    author_id INT AUTO INCREMENT PRIMARY KEY,
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 )
 
 CREATE TABLE Customers (
-    customer_id INT AUTO INCREMENT PRIMARY KEY,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     customer_email VARCHAR(215),
     address TEXT
 )
 
 CREATE TABLE Orders (
-    order_id INT AUTO INCREMENT PRIMARY KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 )
 
 CREATE TABLE Order_Details (
-    order_details_id INT AUTO INCREMENT PRIMARY KEY,
+    order_details_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
---
+++ task3.sql
@@ -0,0 +1,17 @@
+import mysql.connector
+
+try:
+
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        database = "alx_book_store"
+        )
+    cursor = connection.cursor()
+    cursor.execute("SHOW TABLES")
+    connection.commit()
+    print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error as e:
+    print(e)
