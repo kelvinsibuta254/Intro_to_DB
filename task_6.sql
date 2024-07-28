bfc2a8b10c45c1fdda834a63b853a539affeaedb
Kelvin Sibuta <kelvinsibuta254@gmail.com>
2024-07-28 19:00:14 +0300

    alx_book_store

--- task_6.sql
+++ task_6.sql
@@ -10,7 +10,7 @@ try:
         )
     cursor = connection.cursor()
     cursor.execute("USE alx_book_store;")
-    cursor.execute("INSERT INTO customer(customer_id, customer_name, email, address) VALUES(2, "Blessing Malik", "bmalik@sandtech.com", "123 Happiness Ave."), VALUES(3, "Obed Ehoneah", "eobed@sandtech.com", "123 Happiness Ave."), VALUES(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "123 Happiness Ave.") ")
+    cursor.execute("INSERT INTO customer(customer_id, customer_name, email, address) VALUES(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."), VALUES(3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."), VALUES(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.") ")
     connection.commit()
     print("Database 'alx_book_store' created successfully!")
 
